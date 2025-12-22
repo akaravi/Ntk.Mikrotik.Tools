56ad747f285b92200468ee4194dcb6a26e5768c4
karavi <karavi.alireza@gmail.com>
2025-12-22 15:52:42 +0330

    .

---
+++ _publish.cmd
@@ -0,0 +1,23 @@
+git switch main
+git status
+git push
+git pull
+git status
+git merge publish
+git status
+git switch publish
+git status
+git push
+git pull
+git status
+git merge main
+git status
+git merge main
+git status
+git push
+git pull
+git status
+git switch main
+git status
+
+
